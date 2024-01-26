70f16c2642a67148bacda20a06339cc7e217a286
jhraju <jhraju890@gmail.com>
2024-01-26 17:55:48 +0600

    4 types variable  operation

---
+++ operation.js
@@ -0,0 +1,24 @@
+// additional operation ------------
+var eggPrice = 10
+var onion = 10
+
+console.log(eggPrice +onion)
+console.log(eggPrice -onion)
+console.log(eggPrice *onion)
+console.log(eggPrice /onion)
+console.log(10 /3)
+console.log(10%3)  
+console.log(11%7)  
+//  what will be reminder after diving 10by 3^^
+
+// ----------------------------------
+money = 70 ;
+
+var price = 35 ;
+price = price+10;
+var newPrice = price+50;
+var discountPrice = newPrice 10;
+
+console.log(price);
+console.log(newPrice);
+console.log(discountPrice);
