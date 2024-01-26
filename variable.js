e38a5c614e51c5a4673d6409209abc537d7439ad
jhraju <jhraju890@gmail.com>
2024-01-26 10:28:25 +0600

    added variable declare

---
+++ variable.js
@@ -0,0 +1,11 @@
+// vary + able 
+// 5 things to declare a variable  
+
+var weight = 38 ;
+var price = 38 ;
+var money = 1195 ;
+var year = 2005 ;
+var agr ;
+
+console.log(year);
+console.log(money);
