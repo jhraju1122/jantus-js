81b2ee345f24bdb4b0c7d40c7fdfdf9812b988c4
jhraju <jhraju890@gmail.com>
2024-01-26 12:45:24 +0600

    added naming case secsitive variable name pblm solve

--- naming.js
+++ naming.js
@@ -1,7 +1,27 @@
-// 1. no keyword in variable name
+1// 1. no keyword in variable name
 var false = 45 ;
 var var = 50 ;
 
-// no space or gap in  variable name
+2// no space or gap in  variable name
 var isRich = true
 var ishappy = true
+
+3// no quote
+var "address" = " kocu khet"
+
+// 4// can not start with a number 
+var money = 45 ; 
+var money2 = 55 ;
+var 3money = 55 ;
+
+// case sensitive name 
+address
+address
+address
+
+
+// how to write a long variable name 
+var my current home address = "andar killa nabdar bon";
+var my_current_home_address = "andar killa nabdar bon"; snake case 
+var myCurrentHome address = "andar killa nabdar bon"; camel case
+var MyCurrentHomeAddress = "andar killa nabdar bon"; pascal case
