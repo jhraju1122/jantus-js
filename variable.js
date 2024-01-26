45feb9fb84e09844b37232382189a658578931df
jhraju <jhraju890@gmail.com>
2024-01-26 11:00:09 +0600

    added bulian sign and string sign

--- variable.js
+++ variable.js
@@ -7,5 +7,18 @@ var money = 1195 ;
 var year = 2005 ;
 var agr ;
 
+// string data ---------------
+var name = "sadarUddin" ;
+var country = "bd"
+
+// bulian --------------------
+var isRich = true
+var passed = false
+
+
 console.log(year);
 console.log(money);
+console.log(name);
+console.log(isRich);
+console.log( passed);
+console.log(typeof passed);
