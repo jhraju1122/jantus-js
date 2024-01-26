ff18108a5b71712a05f8b58037efab6bbd56d3c0
jhraju <jhraju890@gmail.com>
2024-01-26 15:07:12 +0600

    added number 2 problem solve

---
+++ number.js
@@ -0,0 +1,16 @@
+var orangePrice = 20 ;
+var chocolate = 0.5 ;
+var applePrice =parseInt("26");
+ 
+var first = 0.1;
+var second = 0.5;
+var total = first+second;
+console.log(total.toFixed(4));
+const
+
+
+console.log(orangePrice);
+console.log(typeof orangePrice);
+console.log(applePrice);
+console.log(applePrice+orangePrice+chocolate);
+console.log(typeof applePrice);
